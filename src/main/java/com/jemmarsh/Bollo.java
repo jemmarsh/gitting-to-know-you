6e86d9327e03b8fd297afc7da5e1474ca2d90816
Jeremy Marsh <jmd@jemmarsh.com>
2016-09-30 04:19:02 +0100

    Some source code

---
+++ src/main/java/com/jemmarsh/Bollo.java
@@ -0,0 +1,21 @@
+package com.jemmarsh;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Bollo {
+
+    @RequestMapping("/")
+    String home() {
+        return "What did you expect? A hello?";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Bollo.class, args);
+    }
+
+}
